fe86f042efaea742353c9e916bc4c03031b7d8df
Misty Jenkins <misty.wallace@aggiemail.usu.edu>
2016-10-14 01:03:19 -0600

    Up through section 6 working with unit tests

--- analyze_web_log.py
+++ analyze_web_log.py
@@ -11,7 +11,9 @@ import math
 ## --------- GENERATING FILE NAMES
 
 def generate_file_names(fnpat, rootdir):
-  pass
+  for path, dirlist, filelist in os.walk(rootdir):
+    for file_name in fnmatch.filter(filelist, fnpat):
+        yield os.path.join(path, file_name)
 
 def unit_test_01(fnpat, rootdir):
   for fn in generate_file_names(fnpat, rootdir):
@@ -20,10 +22,14 @@ def unit_test_01(fnpat, rootdir):
 ## ----------- GENERATING INPUT STREAMS & LINES
       
 def generate_input_streams(gen_filenames):
-  pass
+  for filename in gen_filenames:
+      if filename.endswith('.txt'):
+          yield open(filename)
 
 def generate_lines(gen_instreams):
-  pass
+  for stream in gen_instreams:
+      for line in stream:
+          yield line
 
 def unit_test_02(fnpat, rootdir):
   fns = generate_file_names(fnpat, rootdir)
@@ -35,12 +41,21 @@ def unit_test_02(fnpat, rootdir):
 ## ----------- GENERATING TOOPS OF IPS and TRANSFERRED BYTES
 
 def generate_ip_trbts_toops(pat, gen_lines, ip_group_num=1, trbytes_group_num=9):
- pass
+ for line in gen_lines:
+     match = re.match(pat, line)
+     if match:
+         yield (match.group(ip_group_num), match.group(trbytes_group_num))
 
 ip_trbts = {}
 def count_ip_trbts(gen_ip_trbts_toops):
   global ip_trbts
-  pass
+  for toop in gen_ip_trbts_toops:
+      ip = toop[0]
+      bytes = toop[1]
+      if ip in ip_trbts:
+          ip_trbts[ip].append(bytes)
+      else:
+          ip_trbts[ip] = [bytes]
 
 def unit_test_03(fnpat, rootdir):
   logpat = r'^([\d\.\w-]+)\s+(- -)\s+\[(\d{2}\/\w{3}\/\d{4}):(\d{2}:\d{2}:\d{2}).+\]\s+\"(.+)\s+(.+)\s+(.+)\"\s+(\d+)\s+(\d+)$'
@@ -86,24 +101,13 @@ def unit_test_04(fnpat, rootdir, n):
 
 ## comment and uncomment unit tests as appropriate
 if __name__ == '__main__':
-  #unit_test_01(sys.argv[1], sys.argv[2])
-  #unit_test_02(sys.argv[1], sys.argv[2])
-  #unit_test_03(sys.argv[1], sys.argv[2])
+  # unit_test_01(sys.argv[1], sys.argv[2])
+  # unit_test_02(sys.argv[1], sys.argv[2])
+  # unit_test_03(sys.argv[1], sys.argv[2])
+  unit_test_03("small*.txt", "nasa_wlog_data/")
   #unit_test_04(sys.argv[1], sys.argv[2], int(sys.argv[3]))
   #compute_log_stats(sys.argv[1], sys.argv[2])
   #pipe_log_stats(sys.argv[1], sys.argv[2])
   #top_n(gen_log_stats(ip_trbts), int(sys.argv[3]))
   pass
-
-
-
-
-
-
-
-
-
-
-
-
 
